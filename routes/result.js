8410b4d1067526ca534d21c5fd0b263bfae9e8a4
Rafael Câmara <rafaelcamaram@gmail.com>
2019-03-31 22:50:50 -0300

    Adding result route

--- routes/result.js
+++ routes/result.js
@@ -2,40 +2,31 @@ var express = require('express');
 var router = express.Router();
 const glpk = require('../glpk.js');
 
-/* GET users listing. */
+/* GET result listing. */
 router.get('/', function(req, res, next) {
   let lp = {
     name: 'LP',
     objective: {
       direction: glpk.GLP_MAX,
       name: 'obj',
-      vars: [
-        { name: 'x1', coef: 0.6 },
-        { name: 'x2', coef: 0.5 }
-      ]
+      vars: [{ name: 'x1', coef: 0.6 }, { name: 'x2', coef: 0.5 }]
     },
     subjectTo: [
       {
         name: 'cons1',
-        vars: [
-          { name: 'x1', coef: 1.0 },
-          { name: 'x2', coef: 2.0 }
-        ],
+        vars: [{ name: 'x1', coef: 1.0 }, { name: 'x2', coef: 2.0 }],
         bnds: { type: glpk.GLP_UP, ub: 1.0, lb: 0.0 }
       },
       {
         name: 'cons2',
-        vars: [
-          { name: 'x1', coef: 3.0 },
-          { name: 'x2', coef: 1.0 }
-        ],
+        vars: [{ name: 'x1', coef: 3.0 }, { name: 'x2', coef: 1.0 }],
         bnds: { type: glpk.GLP_UP, ub: 2.0, lb: 0.0 }
       }
     ]
   };
-  
-  const response = glpk.solve(lp, glpk.GLP_MSG_ALL)
-  
+
+  const response = glpk.solve(lp, glpk.GLP_MSG_ALL);
+  res.setHeader('Content-Type', 'application/json');
   res.send(response);
 });
 
